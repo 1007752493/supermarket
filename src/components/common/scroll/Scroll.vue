117ae1b3d0963365e8ded4a515fdaed9f3caed2d
TonyWatere <1007752493@qq.com>
2020-05-07 15:30:50 +0800

    初始化项目

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,57 @@
+<template>
+  <!-- ref一般绑定给子组件 -->
+  <div class="wrapper"
+       ref='wrapper'>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BSscroll from 'better-scroll'
+
+export default {
+  name: "Scroll",
+  props: {
+    probeType: {
+      type: Number,
+      default: 0,
+    },
+    pullUpLoad: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      scroll: null
+    }
+  },
+  mounted () {
+    // 创建BScroll
+    this.scroll = new BSscroll(this.$refs.wrapper, {
+      click: true,
+      pullUpLoad: this.pullUpLoad,
+      probeType: this.probeType
+    })
+    //监听滚动的位置
+    this.scroll.on('scroll', (position) => {
+      // console.log(position)
+      this.$emit('scroll', position)
+    })
+    //监听上拉事件
+    this.scroll.on('pullingUp', () => {
+      // console.log('上拉加载更多')
+      this.$emit('pullingUp')
+    })
+    this.scroll.scrollTo(0, 0)
+  },
+  methods: {
+    finishPullUp () { this.scroll.finishPullUp() }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/content/goods/GoodList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="goods">
+    <good-list-item v-for="item in goods"
+                    :goods-item='item'></good-list-item>
+  </div>
+</template>
+
+<script>
+import GoodListItem from './GoodListItem'
+
+export default {
+  name: 'GoodList',
+  components: {
+    GoodListItem
+  },
+  props: {
+    goods: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.goods {
+  display: flex;
+  /* 包裹 */
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+</style>
